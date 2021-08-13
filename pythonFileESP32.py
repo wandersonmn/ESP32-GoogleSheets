05bbed1eaee8a16fa74ae195a824a8bdd45c20a9
wandersonmn <50183447+wandersonmn@users.noreply.github.com>
2021-08-13 17:23:55 -0300

    Add files via upload

--- pythonFileESP32.py
+++ pythonFileESP32.py
@@ -1,17 +1,21 @@
 import prequests
 import ujson
 
+#esp32 must be connected to the internet
+
+deployment_code = "Paste here your google script deployment code"
+
+row_data = {}
+row_data["var1"] = "valor 1"
+row_data["var2"] = 500
+row_data["var3"] = "teste deploy"
+#row_data["varx"] = valuex  (using this you can create more attributes to send)
+
 request_data = ujson.dumps({
-    	"parameters": {
-        "tag": "tagtest",
-        "value":"valuetest",
-        "valor3": 500
-    	}
+    	"parameters": row_data
 })
 
-r = prequests.post("https://script.google.com/macros/s/"deployment_code"/exec",
+r = prequests.post("https://script.google.com/macros/s/" + deployment_code + "/exec",
                   headers = {'content-type': 'application/json'},
                   data = request_data)
-print(r.content)
 r.close()
-
